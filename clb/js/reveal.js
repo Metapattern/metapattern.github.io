b0163e03a3c5df94c6e1b39eb934d77f2b87f1b3
Carson Linforth Bowley <cbowley@gmail.com>
2015-06-11 18:39:26 -0700

    fixed full width issue

--- clb/js/reveal.js
+++ clb/js/reveal.js
@@ -39,7 +39,7 @@
 			height: 700,
 
 			// Factor of the display size that should remain empty around the content
-			margin: 0.4,
+			margin: 0.2,
 
 			// Bounds for smallest/largest possible scale to apply to content
 			minScale: 0.6,
